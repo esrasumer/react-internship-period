4b4cc7e41706f07a07bebe5871708959c0e38f90
esrasumer <esrasumer28@gmail.com>
2024-11-01 17:39:04 +0300

    I solved js problems

--- src/components/jsProblems/problem.js
+++ src/components/jsProblems/problem.js
@@ -1,7 +1,18 @@
 //reduce: dizinin her değeri için bir işlev yürütür
-
-function SayilarinToplami(arr){
-    return arr.reduce((acc,curr) => acc+curr)
+function SayilarinToplami(arr) {
+    return arr.reduce((acc, curr) => acc + curr)
 }
+console.log(SayilarinToplami([1, 2, 3, 4]));
 
-console.log(SayilarinToplami([1,2,3,4]));
+//filter: 
+function PozitifSayilarinToplami(arr) {
+    return arr.filter((x) => x > 0).reduce((acc, curr) => acc + curr, 0)
+}
+console.log(PozitifSayilarinToplami([1, 5, 2, 8, 16, -16, -100000]))
+
+//polindrom:
+function tersCevirme(str) {
+    let tersYazi = str.split('').reverse().join('')
+    return str === tersYazi ? true : false
+}
+console.log(tersCevirme('ada'))
