23310936b75f645fb3087ec62adbcf899a2899de
esrasumer <esrasumer28@gmail.com>
2024-11-02 23:57:09 +0300

    I solved js problems

--- src/components/jsProblems/problem.js
+++ src/components/jsProblems/problem.js
@@ -15,4 +15,25 @@ function tersCevirme(str) {
     let tersYazi = str.split('').reverse().join('')
     return str === tersYazi ? true : false
 }
+console.log(tersCevirme('esra'))
 console.log(tersCevirme('ada'))
+
+//for if
+
+function minVeMaxSayiBulma(arr) {
+    if (arr.length === 0) {
+        return "boş küme"
+    }
+    let minSayi = arr[0];
+    let maxSayi = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < minSayi) {
+            minSayi = arr[i]
+        }
+        if (arr[i] > maxSayi) {
+            maxSayi = arr[i]
+        }
+    }
+    return [minSayi, maxSayi];
+}
+console.log(minVeMaxSayiBulma([5, 8, 0, 2134, -9]))
